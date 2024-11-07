ac8d8a6ad9f7e25f7d948aa4b759af76f004883c
Leonardo <leonardotkimura@gmail.com>
2024-11-07 00:45:13 +0000

    add setup

--- logserver/start.sh
+++ logserver/start.sh
@@ -1,11 +1,13 @@
-wget -O data/2022_round_2.zip https://github.com/larc-logs-transparentes/tdsc/releases/download/v0.0.1-dev/2022_round_2.zip data
-unzip data/2022_round_2.zip 
+mkdir data
+cd data
+wget -O 2022_round_2.zip https://github.com/larc-logs-transparentes/tdsc/releases/download/v0.0.1-dev/2022_round_2.zip
+unzip 2022_round_2.zip 
 
-docker compose up -d
+# docker compose up -d
 
-python3 -m venv venv
-source ./venv/bin/activate
+# python3 -m venv venv
+# source ./venv/bin/activate
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
-python scripts/construct_trees.py
+# python scripts/construct_trees.py
