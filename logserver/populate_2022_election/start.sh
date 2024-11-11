4f264543471811327b40cc63ace3e60796669d5a
root <leoanrdo>
2024-11-11 17:12:14 +0000

    correct status path

--- logserver/populate_2022_election/start.sh
+++ logserver/populate_2022_election/start.sh
@@ -1,8 +1,9 @@
 # Download 2022 election data (adapted from https://bit.ly/3NXoUyT)
 mkdir data
 cd data
-wget -O 2022_round_2.zip https://github.com/larc-logs-transparentes/tdsc/releases/download/v0.0.1-dev/2022_round_2.zip
-unzip 2022_round_2.zip 
+# wget -O 2022_round_2.zip https://github.com/larc-logs-transparentes/tdsc/releases/download/v0.0.1-dev/2022_round_2.zip
+# unzip 2022_round_2.zip 
+cd ../
 
 
 # Activate Python virtual environment
